4840238a1236e5525440564893676444e77e26a1
ramahisyam <ramahisyam1@gmail.com>
2022-01-12 09:06:07 +0700

    Resolve Conflict

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,8 +44,7 @@
                         </g>
                     </svg>
                 </div>
-
-                
+       
 
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
