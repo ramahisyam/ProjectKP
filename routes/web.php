2c9dbb52055345facf13ecf816f1d67ff4587c9e
dwinuragustina <dwinuragustina92@gmail.com>
2022-01-19 13:33:51 +0700

    Temporary push

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return view('Backroom.detail');
+
+});*/
+
+Route::resource('blog', BlogController::class);
+Route::resource('users', UserController::class);
